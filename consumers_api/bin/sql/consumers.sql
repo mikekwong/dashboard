ed47e51441fd902cbde9d14a85cfa83e7a61c05b
Mike Wong <wongmdesign@gmail.com>
2019-07-25 17:10:16 -0400

    added post request

--- consumers_api/bin/sql/consumers.sql
+++ consumers_api/bin/sql/consumers.sql
@@ -1,11 +1,11 @@
 CREATE TABLE consumers (
   id serial,
   email varchar(50),
-  "phone number" varchar(14),
-  "City/State/Zip" varchar(50),
-  textalerts BOOLEAN,
-  "coupon selected" BOOLEAN
+  phone_number varchar(14),
+  city_state_zip varchar(50),
+  textalerts varchar(3),
+  coupon_selected varchar(25)
 );
 
-INSERT INTO consumers (email, "phone number", "City/State/Zip", textalerts, "coupon selected")
-    VALUES ('john.doe@gmail.com', '(123) 567 8901', 'New York, NY 10013', TRUE, FALSE);
+INSERT INTO consumers (email, phone_number, city_state_zip, textalerts, coupon_selected)
+    VALUES ('john.doe@gmail.com', '(123) 567 8901', 'New York, NY 10013', 'yes', 'free upgrade');
--- consumers_api/routes/consumers.js
+++ consumers_api/routes/consumers.js
@@ -4,10 +4,28 @@ const pool = require('../db')
 const router = Router()
 
 router.get('/', async (request, response, next) => {
-  await pool.query('SELECT * FROM consumers ORDER BY id ASC', (err, res) => {
+  await pool.query('SELECT * FROM consumers', (err, res) => {
     if (err) return next(err)
     response.json(res.rows)
   })
+})
+
+router.post('/', (request, response, next) => {
+  const {
+    email,
+    phone_number,
+    city_state_zip,
+    textalerts,
+    coupon_selected,
+  } = request.body
+  pool.query(
+    'INSERT INTO consumers (email, phone_number, city_state_zip, textalerts, coupon_selected) VALUES ($1, $2, $3, $4, $5)',
+    [email, phone_number, city_state_zip, textalerts, coupon_selected],
+    (err, res) => {
+      if (err) return next(err)
+      response.redirect('/consumers')
+    }
+  )
 })
 
 module.exports = router
